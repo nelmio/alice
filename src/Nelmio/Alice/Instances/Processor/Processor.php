3a9bb07c15f92d40067d342235c86ed5cdb480a5
Tim Shelburne <shelburt02@gmail.com>
2014-03-10 18:05:42 -0500

    moved inline closure in Processor#parse to an entirely separate method

--- src/Nelmio/Alice/Instances/Processor/Processor.php
+++ src/Nelmio/Alice/Instances/Processor/Processor.php
@@ -113,51 +113,15 @@ class Processor {
 			return $data;
 		}
 
-		$that = $this;
-		// replaces a placeholder by the result of a ->fake call
-		$replacePlaceholder = function ($matches) use ($variables, $that) {
-			$args = isset($matches['args']) && '' !== $matches['args'] ? $matches['args'] : null;
-
-			if (!$args) {
-				return $that->fake($matches['name'], $matches['locale']);
-			}
-
-			// replace references to other variables in the same object
-			$args = preg_replace_callback('{\{?\$([a-z0-9_]+)\}?}i', function ($match) use ($variables) {
-				if (array_key_exists($match[1], $variables)) {
-					return '$variables['.var_export($match[1], true).']';
-				}
-
-				return $match[0];
-			}, $args);
-
-			// replace references to other objects
-			$args = preg_replace_callback('{(?:\b|^)(?:(?<multi>\d+)x )?(?<!\\\\)@(?<reference>[a-z0-9_.*-]+)(?:\->(?<property>[a-z0-9_-]+))?(?:\b|$)}i', function ($match) use ($that) {
-				$multi    = ('' !== $match['multi']) ? $match['multi'] : null;
-				$property = isset($match['property']) ? $match['property'] : null;
-				if (strpos($match['reference'], '*')) {
-					return '$that->objects->random(' . var_export($match['reference'], true) . ', ' . var_export($multi, true) . ', ' . var_export($property, true) . ')';
-				}
-				if (null !== $multi) {
-					throw new \UnexpectedValueException('To use multiple references you must use a mask like "'.$match['multi'].'x @user*", otherwise you would always get only one item.');
-				}
-				return '$that->objects->find(' . var_export($match['reference'], true) . ', ' . var_export($property, true) . ')';
-			}, $args);
-
-			$locale = var_export($matches['locale'], true);
-			$name = var_export($matches['name'], true);
-
-			return eval('return $that->fake(' . $name . ', ' . $locale . ', ' . $args . ');');
-		};
-
 		// format placeholders without preg_replace if there is only one to avoid __toString() being called
 		$placeHolderRegex = '<(?:(?<locale>[a-z]+(?:_[a-z]+)?):)?(?<name>[a-z0-9_]+?)\((?<args>(?:[^)]*|\)(?!>))*)\)>';
 		if (preg_match('#^'.$placeHolderRegex.'$#i', $data, $matches)) {
-			$data = $replacePlaceholder($matches);
+			$data = $this->replacePlaceholder($matches, $variables);
 		} else {
 			// format placeholders inline
-			$data = preg_replace_callback('#'.$placeHolderRegex.'#i', function ($matches) use ($replacePlaceholder) {
-				return $replacePlaceholder($matches);
+			$that = $this;
+			$data = preg_replace_callback('#'.$placeHolderRegex.'#i', function ($matches) use ($that, $variables) {
+				return $that->replacePlaceholder($matches, $variables);
 			}, $data);
 		}
 
@@ -181,6 +145,44 @@ class Processor {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * replaces a placeholder by the result of a ->fake call
+	 */ 
+	private function replacePlaceholder($matches, array $variables) {
+		$args = isset($matches['args']) && '' !== $matches['args'] ? $matches['args'] : null;
+
+		if (!$args) {
+			return $this->fake($matches['name'], $matches['locale']);
+		}
+
+		// replace references to other variables in the same object
+		$args = preg_replace_callback('{\{?\$([a-z0-9_]+)\}?}i', function ($match) use ($variables) {
+			if (array_key_exists($match[1], $variables)) {
+				return '$variables['.var_export($match[1], true).']';
+			}
+
+			return $match[0];
+		}, $args);
+
+		// replace references to other objects
+		$args = preg_replace_callback('{(?:\b|^)(?:(?<multi>\d+)x )?(?<!\\\\)@(?<reference>[a-z0-9_.*-]+)(?:\->(?<property>[a-z0-9_-]+))?(?:\b|$)}i', function ($match) {
+			$multi    = ('' !== $match['multi']) ? $match['multi'] : null;
+			$property = isset($match['property']) ? $match['property'] : null;
+			if (strpos($match['reference'], '*')) {
+				return '$this->objects->random(' . var_export($match['reference'], true) . ', ' . var_export($multi, true) . ', ' . var_export($property, true) . ')';
+			}
+			if (null !== $multi) {
+				throw new \UnexpectedValueException('To use multiple references you must use a mask like "'.$match['multi'].'x @user*", otherwise you would always get only one item.');
+			}
+			return '$this->objects->find(' . var_export($match['reference'], true) . ', ' . var_export($property, true) . ')';
+		}, $args);
+
+		$locale = var_export($matches['locale'], true);
+		$name = var_export($matches['name'], true);
+
+		return eval('return $this->fake(' . $name . ', ' . $locale . ', ' . $args . ');');
 	}
 
 	private function fake($formatter, $locale = null)
