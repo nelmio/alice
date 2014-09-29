1558635f4641439fa84d31fd2b03d5f75bf74cfe
Jordi Boggiano <j.boggiano@seld.be>
2014-09-29 21:37:24 +0100

    Remove dead code, fixes #111

--- src/Nelmio/Alice/Loader/Base.php
+++ src/Nelmio/Alice/Loader/Base.php
@@ -420,12 +420,7 @@ class Base implements LoaderInterface
         $flags = array();
         if (preg_match('{^(.+?)\s*\((.+)\)$}', $key, $matches)) {
             foreach (preg_split('{\s*,\s*}', $matches[2]) as $flag) {
-                $val = true;
-                if ($pos = strpos($flag, ':')) {
-                    $flag = trim(substr($flag, 0, $pos));
-                    $val = trim(substr($flag, $pos+1));
-                }
-                $flags[$flag] = $val;
+                $flags[$flag] = true;
             }
             $key = $matches[1];
         }
