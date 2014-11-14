1d21752b15a34fd62bacf57665583cbdfe0cfa0c
Maxim Krizhanovsky <maxim@picasse.com>
2014-11-14 15:24:19 +0200

    Add tests

---
+++ tests/Nelmio/Alice/fixtures/includes/file1.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+  'include' => array(
+    'file2.php'
+  ),
+  'Nelmio\Alice\fixtures\Shop' => array(
+    'shop1' => array(
+      'domain' => '<word()>'
+    ),
+    'shop2' => array(
+      'domain' => 'amazon.com'
+    )
+  )
+);
