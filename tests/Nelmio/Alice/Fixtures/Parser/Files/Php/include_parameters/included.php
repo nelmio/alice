fd6b493667d1918a19cec3bbb515f3766ea9097d
Théo FIDRY <theo.fidry@gmail.com>
2016-05-14 13:50:32 +0100

    [v2] 	Port parameters feature to PHP loader (#341)

---
+++ tests/Nelmio/Alice/Fixtures/Parser/Files/Php/include_parameters/included.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Alice package.
+ *
+ *  (c) Nelmio <hello@nelm.io>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+return [
+    'parameters' => [
+        'foo' => 'boo',
+        'ping' => 'pong',
+    ],
+];
