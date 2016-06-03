d4209fd6366c54b6e4fca76cc8a2a72bbb12a1c9
Théo FIDRY <theo.fidry@gmail.com>
2016-06-03 22:29:17 +0100

    WIP

---
+++ src/Nelmio/Alice/LoaderInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Alice package.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\Alice;
+
+use Nelmio\Alice\Throwable\LoadingThrowable;
+
+/**
+ * Main interface of the library.
+ */
+interface LoaderInterface
+{
+    /**
+     * Loads a fixture file.
+     *
+     * @param string $file       File to load.
+     * @param array  $parameters Additional parameters to inject.
+     * @param array  $objects    Additional objects to inject.
+     *
+     * @throws LoadingThrowable
+     *
+     * @return ObjectSet Contains the list of objects and parameters loaded and injected.
+     */
+    public function load(string $file, array $parameters = [], array $objects = []): ObjectSet;
+}
---
+++ src/Nelmio/Alice/ObjectSet.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Alice package.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\Alice;
+
+/**
+ * Value object containing the parameters and objects built from the loaded and injected ones.
+ */
+final class ObjectSet
+{
+    //TODO
+}
