14311a6494fcc2278e687efba920c7ac06d93de2
Théo FIDRY <theo.fidry@gmail.com>
2016-07-28 14:49:45 +0100

    Flatten directories
    The directory structure has not been changed although it is possible with Composer PSR-4 autoloader. This has not been done in 2.x to avoid breaking to many PRs and links, but this can be done in 3.x now.

---
+++ src/FileLocatorInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Alice package.
+ *  
+ * (c) Nelmio <hello@nelm.io>
+ *  
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\Alice;
+
+use Nelmio\Alice\Exception\FileLocator\FileNotFoundException;
+
+interface FileLocatorInterface
+{
+    /**
+     * @param string      $name        Name of the file to locate
+     * @param string|null $currentPath Path in which the file can be found
+     *
+     * @throws FileNotFoundException
+     * @throws \InvalidArgumentException
+     *                                 
+     * @return string The full path to the file
+     */
+    public function locate(string $name, string $currentPath = null): string;
+}
---
+++ src/ParserInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Alice package.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nelmio\Alice;
+
+use Nelmio\Alice\Throwable\ParseThrowable;
+
+interface ParserInterface
+{
+    /**
+     * Parses the given file and returns an array of data.
+     *
+     * @param string $file File path
+     *
+     * @throws ParseThrowable
+     *
+     * @return array
+     */
+    public function parse(string $file): array;
+}
