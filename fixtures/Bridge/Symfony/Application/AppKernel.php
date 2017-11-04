358543e6965bf5f8bf4c0b3ff29bc1c8092ef79c
Théo FIDRY <theo.fidry@gmail.com>
2017-11-04 18:38:56 +0000

    Add Symfony 4.0 support (#834)

--- fixtures/Bridge/Symfony/Application/AppKernel.php
+++ fixtures/Bridge/Symfony/Application/AppKernel.php
@@ -16,6 +16,10 @@ namespace Nelmio\Alice\Bridge\Symfony\Application;
 use Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
@@ -45,13 +49,32 @@ class AppKernel extends Kernel
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $config = (null === $this->config) ? __DIR__.'/config.yml' : $this->config;
 
         $loader->load($config);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new class implements CompilerPassInterface {
+            public function process(ContainerBuilder $container)
+            {
+                foreach ($container->getDefinitions() as $id => $definition) {
+                    $definition->setPublic(true);
+                }
+
+                foreach ($container->getAliases() as $id => $definition) {
+                    $definition->setPublic(true);
+                }
+            }
+        }, PassConfig::TYPE_OPTIMIZE);
     }
 
     public function setConfigurationResource(string $resource)
