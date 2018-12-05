ce1f92407297bc0cd465b3e6d7ce17c6b99b629a
jmsche <jonathan.scheiber@siep.be>
2018-12-05 13:08:00 +0100

    Fixed root_node deprecation with Symfony 4.2

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -27,8 +27,14 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nelmio_alice');
+        $treeBuilder = new TreeBuilder('nelmio_alice');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('nelmio_alice');
+        }
+
         $rootNode
             ->children()
                 ->scalarNode('locale')
