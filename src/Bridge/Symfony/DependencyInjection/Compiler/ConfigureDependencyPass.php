a96a5b7dbd1947839908454e03bf464fce83ff96
Maxime CORNET <maximecornet@outlook.fr>
2022-11-07 20:40:26 +0100

    Add CompilerPass to check if framework_bundle.property_access is enabled
    This allows to have a more user-friendly error when the framework_bundle.property_access is disabled.

---
+++ src/Bridge/Symfony/DependencyInjection/Compiler/ConfigureDependencyPass.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Alice package.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Nelmio\Alice\Bridge\Symfony\DependencyInjection\Compiler;
+
+use RuntimeException;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class ConfigureDependencyPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->has('property_accessor')) {
+            throw new RuntimeException('NelmioAliceBundle requires framework_bundle.property_access to be enabled.');
+        }
+    }
+}
