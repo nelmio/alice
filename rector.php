45729e8c5ca8ea24e9c4f180c0d7be85b19a39ce
Théo FIDRY <theo.fidry@gmail.com>
2025-12-06 19:12:25 +0100

    test: Enable PHPUnit preset for rector

--- rector.php
+++ rector.php
@@ -9,6 +9,10 @@ use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php80\Rector\Ternary\GetDebugTypeRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
 use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\YieldDataProviderRector;
+use Rector\PHPUnit\CodeQuality\Rector\MethodCall\AssertEqualsToSameRector;
+use Rector\Privatization\Rector\Class_\FinalizeTestCaseClassRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -22,11 +26,23 @@ return RectorConfig::configure()
     ->withTypeCoverageLevel(0)
     ->withDeadCodeLevel(0)
     ->withCodeQualityLevel(0)
+    ->withPreparedSets(
+        phpunitCodeQuality: true,
+    )
     ->withSkip([
+        AssertEqualsToSameRector::class,
         ClassPropertyAssignToConstructorPromotionRector::class => __DIR__.'/fixtures',
         ClosureToArrowFunctionRector::class,
+        FinalizeTestCaseClassRector::class => [
+            __DIR__.'/tests/FixtureBuilder/ExpressionLanguage/Lexer/LexerIntegrationTest.php',
+            __DIR__.'/tests/FixtureBuilder/ExpressionLanguage/Parser/ParserIntegrationTest.php',
+            __DIR__.'/tests/Generator/Resolver/ParameterResolverIntegrationTest.php',
+            __DIR__.'/tests/Loader/LoaderIntegrationTest.php',
+        ],
         GetDebugTypeRector::class,
         NullToStrictStringFuncCallArgRector::class,
         ReadOnlyPropertyRector::class => __DIR__.'/fixtures',
+        PreferPHPUnitThisCallRector::class,
         StringClassNameToClassConstantRector::class,
+        YieldDataProviderRector::class,
     ]);
