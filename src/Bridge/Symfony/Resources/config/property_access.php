5a670dc467739e1732c9f292f5aae816cf79cbc5
Théo FIDRY <theo.fidry@gmail.com>
2025-12-09 12:47:01 +0100

    Fix CS

--- src/Bridge/Symfony/Resources/config/property_access.php
+++ src/Bridge/Symfony/Resources/config/property_access.php
@@ -1,8 +1,19 @@
 <?php
 
+/*
+ * This file is part of the Alice package.
+ *
+ * (c) Nelmio <hello@nelm.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $container) {
+return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
     $services->alias('nelmio_alice.property_accessor', 'nelmio_alice.property_accessor.std');
